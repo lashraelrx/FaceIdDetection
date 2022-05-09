2c9af2a978c1aabe3998ffc0a399cbc93a8107a5
Ahmet Batuhan Yılmaz <59421016+lashraelrx@users.noreply.github.com>
2022-05-09 21:04:46 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace FaceDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idButton = new System.Windows.Forms.Button();
+            this.faceButton = new System.Windows.Forms.Button();
+            this.folderButton = new System.Windows.Forms.Button();
+            this.folderBox = new System.Windows.Forms.TextBox();
+            this.logBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // idButton
+            // 
+            this.idButton.Location = new System.Drawing.Point(12, 7);
+            this.idButton.Name = "idButton";
+            this.idButton.Size = new System.Drawing.Size(162, 186);
+            this.idButton.TabIndex = 0;
+            this.idButton.Text = "ID Detection Mode";
+            this.idButton.UseVisualStyleBackColor = true;
+            this.idButton.Click += new System.EventHandler(this.idButton_Click);
+            // 
+            // faceButton
+            // 
+            this.faceButton.Location = new System.Drawing.Point(12, 205);
+            this.faceButton.Name = "faceButton";
+            this.faceButton.Size = new System.Drawing.Size(162, 186);
+            this.faceButton.TabIndex = 1;
+            this.faceButton.Text = "Face Detection Mode";
+            this.faceButton.UseVisualStyleBackColor = true;
+            this.faceButton.Click += new System.EventHandler(this.faceButton_Click);
+            // 
+            // folderButton
+            // 
+            this.folderButton.Location = new System.Drawing.Point(12, 415);
+            this.folderButton.Name = "folderButton";
+            this.folderButton.Size = new System.Drawing.Size(110, 23);
+            this.folderButton.TabIndex = 2;
+            this.folderButton.Text = "Select Folder";
+            this.folderButton.UseVisualStyleBackColor = true;
+            this.folderButton.Click += new System.EventHandler(this.folderButton_Click);
+            // 
+            // folderBox
+            // 
+            this.folderBox.Enabled = false;
+            this.folderBox.Location = new System.Drawing.Point(128, 418);
+            this.folderBox.Name = "folderBox";
+            this.folderBox.Size = new System.Drawing.Size(498, 20);
+            this.folderBox.TabIndex = 3;
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(180, 12);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(446, 379);
+            this.logBox.TabIndex = 4;
+            this.logBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 450);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.folderBox);
+            this.Controls.Add(this.folderButton);
+            this.Controls.Add(this.faceButton);
+            this.Controls.Add(this.idButton);
+            this.Name = "Form1";
+            this.Text = "Jarvis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button idButton;
+        private System.Windows.Forms.Button faceButton;
+        private System.Windows.Forms.Button folderButton;
+        private System.Windows.Forms.TextBox folderBox;
+        private System.Windows.Forms.RichTextBox logBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace FaceDetection
+{
+    public partial class Form1 : Form
+    {
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("necmi.xml");
+        public string FolderPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void folderButton_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if(result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    FolderPath = fbd.SelectedPath;
+                    folderBox.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void idButton_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(FolderPath))
+            {
+                MessageBox.Show("You must select a folder first.");
+            }
+        }
+
+        private void faceButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(FolderPath))
+            {
+                MessageBox.Show("You must select a folder first.");
+            }
+            var Photos = GatherPhotos();
+            for (var i = 0; i < Photos.Count; i++)
+            {
+                Bitmap img = new Bitmap(Image.FromFile(Photos[i]));
+                BitmapData bitmapData = img.LockBits(new Rectangle(0, 0, img.Width, img.Height), ImageLockMode.ReadOnly, img.PixelFormat);
+                //Image<Bgr, byte> grayImage = new Image<Bgr, byte>(img.Width, img.Height, bitmapData.Stride, bitmapData.Scan0);
+                img.UnlockBits(bitmapData);
+
+                Image<Bgr, byte> grayImage = new Image<Bgr, byte>(img);
+
+                Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(image: grayImage, scaleFactor: 1.2, minNeighbors: 1);
+                //Console.WriteLine(rectangles.Count().ToString() + " " + Photos[i]);
+                if(rectangles.Count().ToString() == "0")
+                {
+                    //Console.Write(Photos[i] + " is not a valid face.");
+                    Append(Photos[i] + " is not valid");
+                    //continue;
+                } else
+                {
+                    Append(Photos[i] + " is valid");
+                }
+                /*/
+                foreach(Rectangle rectangle in rectangles)
+                {
+                    using (Graphics graphics = Graphics.FromImage(img))
+                    using(Pen pen = new Pen(Brushes.Red))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
+                /*/
+            }
+        }
+        private List<string> GatherPhotos()
+        {
+            var files = Directory.GetFiles(@"" + FolderPath, "*.*", SearchOption.TopDirectoryOnly);
+            List<string> imageFiles = new List<string>();
+            foreach(string filename in files)
+            {
+                if(Regex.IsMatch(filename, @"\.jpg$|\.jpeg$"))
+                {
+                    imageFiles.Add(filename);
+                }
+            }
+            return imageFiles;
+        }
+        private void Append(String text)
+        {
+            logBox.Text += "\n" + text;
+        }
+    }
+    
+}
